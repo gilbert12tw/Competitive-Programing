2355acb5972f78cbd66a10898f4fb2fe8e960fe4
Littlepants <gilbert12.tw@gmail.com>
2023-07-09 14:51:23 +0800

    7/9 update

--- tmp/pA.cpp
+++ tmp/pA.cpp
@@ -1,95 +1,69 @@
-#include<bits/stdc++.h>
-#define loli
+#include<iostream>
+#include<list>
+#include<cstring>
+
 using namespace std;
-typedef long long ll;
-#define int ll
-#define pii pair<int, int>
-#define X first
-#define Y second
-#define F first
-#define S second
-#define SZ(a) ((int)a.size())
-#define ALL(v) v.begin(), v.end()
-#define pb push_back
-#define eb emplace_back
-#define push emplace
-#define lb(x, v) lower_bound(ALL(x), v)
-#define ub(x, v) upper_bound(ALL(x), v)
-#define re(x) reverse(ALL(x))
-#define uni(x) x.resize(unique(ALL(x)) - x.begin())
-#define inf 1000000000
-#define INF 1000000000000000000
-#define mod 1000000007
-#define get_bit(x, y) ((x>>y)&1)
-#define mkp make_pair
-#define IO ios_base::sync_with_stdio(0); cin.tie(0);
-void abc() {cout << endl;}
-template <typename T, typename ...U> void abc(T a, U ...b) {
-    cout << a << ' ', abc(b...);
-}
-template <typename T> void printv(T l, T r) {
-    for (; l != r; ++l) cout << *l << " \n"[l + 1 == r];
-}
-template <typename A, typename B> istream& operator >> (istream& o, pair<A, B> &a) {
-    return o >> a.X >> a.Y;
-}
-template <typename A, typename B> ostream& operator << (ostream& o, pair<A, B> a) {
-    return o << '(' << a.X << ", " << a.Y << ')';
-}
-template <typename T> ostream& operator << (ostream& o, vector<T> a) {
-    bool is = false;
-    if (a.empty()) return o << "{}";
-    for (T i : a) {o << (is ? ' ' : '{'), is = true, o << i;}
-    return o << '}';
-}
-#ifdef loli
-#define test(args...) abc("[" + string(#args) + "]", args)
-#else
-#define test(args...) void(0)
-#endif
-
-const int mxN = 2e6 + 5;
-
-int n, k, m;
-vector<int> seg[mxN];
-
-int sol() {
-    multiset<int> st;
-    int res = 0;
-    for (int i = 0; i < k; i++) st.insert(0);
-    for (int r = 1; r <= 1000000; r++) {
-        for (int l : seg[r]) {
-            auto it = st.lower_bound(l);
-            if (it == st.begin()) continue;
-            it = prev(it);
-            st.erase(it);
-            st.insert(r);
-            res++;
-        }
-    } 
-    return res;
-}
-
-inline void solve() {
-    cin >> n >> k >> m;
-    for (int i = 0; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        seg[v].eb(u);
-    }
-
-    while (m--) {
-        int q, a, b;
-        cin >> q >> a >> b;
-        if (q == 2) {
-            cout << sol() << '\n';
-        } else {
-            seg[b].eb(a);
-        }
-    }
-}
 
 signed main() {
-	IO;	
-	solve();	
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    list<int> mylist = {1, 2, 3};
+    mylist.push_front(10); // mylist = {10, 1, 2, 3}
+    mylist.pop_back();     // mylist = {10, 1, 2}
+
+   for (list<int>::iterator it = mylist.begin(); it != mylist.end(); it++) {
+      if (*it == 1) { // delete 1 in the list
+        it = mylist.erase(it); 
+      }
+   }
+   // mylist = {10, 2}
+
+   for (list<int>::iterator it = mylist.begin(); it != mylist.end(); it++) {
+      if (*it == 10) { // insert -1 before 10
+        mylist.insert(it, -1); 
+      }
+   }
+   // mylist {-1, 10, 2}
+
+   for (auto it : mylist) cout << it << ' ';
+
+
+
+    return 0;
+    int n; cin >> n;
+    list<int> li;
+    while (n--) {
+        string opt; cin >> opt;
+        int i;
+        if (opt[0] == 'I') {
+            cin >> i;
+            if (opt[1] == 'H') {   // "IH"
+                li.push_front(i);
+            } else {               // "IT"
+                li.push_back(i);
+            }
+        } else if (opt[0] == 'R') {
+            if (li.empty()) // do nothing
+                continue; 
+            if (opt[1] == 'H') {   // "RH"
+                li.pop_front(); 
+            } else {               // "RT"
+                li.pop_back(); 
+            }
+        } else if (opt[0] == 'S') {// "S"
+            cin >> i;
+            if (li.empty()) { cout << "E\n"; continue;}
+            // traverse the linked list
+            int ok = 0;
+            for (auto it : li) 
+                if (it == i) ok = 1;
+
+            cout << (ok ? "Y" : "N") << '\n';
+        } else {                   // "O"
+            if (li.empty()) { cout << "E\n"; continue;}
+            for (auto it : li) cout << it << ' ';
+            cout << '\n';
+        }
+    }
 }
+
--- tmp/test.cpp
+++ tmp/test.cpp
@@ -1,86 +1,27 @@
 #include<bits/stdc++.h>
-#define loli
 using namespace std;
-typedef long long ll;
-#define int ll
-#define pii pair<int, int>
-#define X first
-#define Y second
-#define F first
-#define S second
-#define SZ(a) ((int)a.size())
-#define ALL(v) v.begin(), v.end()
-#define pb push_back
-#define eb emplace_back
-#define push emplace
-#define lb(x, v) lower_bound(ALL(x), v)
-#define ub(x, v) upper_bound(ALL(x), v)
-#define re(x) reverse(ALL(x))
-#define uni(x) x.resize(unique(ALL(x)) - x.begin())
-#define inf 1000000000
-#define INF 1000000000000000000
-#define mod 1000000007
-#define get_bit(x, y) ((x>>y)&1)
-#define mkp make_pair
-#define IO ios_base::sync_with_stdio(0); cin.tie(0);
-void abc() {cout << endl;}
-template <typename T, typename ...U> void abc(T a, U ...b) {
-    cout << a << ' ', abc(b...);
-}
-template <typename T> void printv(T l, T r) {
-    for (; l != r; ++l) cout << *l << " \n"[l + 1 == r];
-}
-template <typename A, typename B> istream& operator >> (istream& o, pair<A, B> &a) {
-    return o >> a.X >> a.Y;
-}
-template <typename A, typename B> ostream& operator << (ostream& o, pair<A, B> a) {
-    return o << '(' << a.X << ", " << a.Y << ')';
-}
-template <typename T> ostream& operator << (ostream& o, vector<T> a) {
-    bool is = false;
-    if (a.empty()) return o << "{}";
-    for (T i : a) {o << (is ? ' ' : '{'), is = true, o << i;}
-    return o << '}';
-}
-#ifdef loli
-#define test(args...) abc("[" + string(#args) + "]", args)
-#else
-#define test(args...) void(0)
-#endif
 
-const int mxN = 2e6 + 5;
+int main() {
+    int n; 
+    cin >> n;
 
-int n, k, m;
-pii seg[mxN];
+    if (n == 1) {
+        cout << 1 << '\n';
+        return 0;
+    }
 
-int sol() {
-    multiset<int> st;
-    int res = 0;
-    sort(seg, seg + n, [](pii a, pii b) {
-        return a.S < b.S;
-    });
-
-    for (int i = 0; i < k; i++) st.insert(0);
+    list<int> li;
+    for (int i = 1; i <= n; i++)
+        li.push_back(i);
+    
+    auto it = li.begin();
+    it++;
     for (int i = 0; i < n; i++) {
-        int l = seg[i].F, r = seg[i].S;
-        auto it = st.lower_bound(l);
-        if (it == st.begin()) continue;
-        it = prev(it);
-        st.erase(it);
-        st.insert(r);
-        res++;
-    } 
-    return res;
-}
+        cout << *it << ' ';
+        it = li.erase(it);
 
-inline void solve() {
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) cin >> seg[i].F;
-    for (int i = 0; i < n; i++) cin >> seg[i].S;
-    cout << sol();
-}
-
-signed main() {
-	IO;	
-	solve();	
+        if (it == li.end()) it = li.begin(); 
+        it++;
+        if (it == li.end()) it = li.begin(); 
+    }
 }
